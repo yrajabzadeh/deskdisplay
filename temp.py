dacd09fd6cc71464b2f575ab037451a90cce3380
yrajabzadeh <yousefr@gmail.com>
2017-06-05 17:38:20 -0700

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,9 @@
+#temp.py
+#include <Python.h>
+#   display current temperature and forecast
+
+def main():
+temp=input(pull_node)
+print ("The Current Temperature:")
+print (celcius"degrees", fahrenheit"degrees")
+main()
